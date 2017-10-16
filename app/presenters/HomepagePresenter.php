0cdf960a6ac615b0bcbed02b3b1f53341b2ca599
Filip Stransky <stranskyfilip@seznam.cz>
2017-10-16 21:06:35 +0200

    pocatecni commit

---
+++ app/presenters/HomepagePresenter.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette;
+use Nette\Application\UI\Form;
+
+
+class HomepagePresenter extends Nette\Application\UI\Presenter
+{
+
+    private $database;
+
+    public function __construct(Nette\Database\Context $database)
+    {
+        $this->database = $database;
+    }
+
+    public function renderDefault()
+    {
+        $this->template->posts = $this->database->query('SELECT * FROM knihy k JOIN zanry z ON k.zanr_id=z.zan_id ORDER BY nazev_zanru DESC');
+    }
+
+    protected function createComponentCommentForm()
+    {
+
+        $zanry = $this->database->fetchPairs('SELECT zan_id, nazev_zanru FROM zanry');;
+
+        $form = new Form; // means Nette\Application\UI\Form
+
+        $form->addText('nazev', 'Název:')->setRequired();
+
+        $form->addText('autor', 'Autor:')->setRequired();
+
+        $form->addSelect('zanr_id', 'Žánr:', $zanry)->setPrompt('Zvolte žánr')->setRequired();
+
+        $form->addSubmit('send', 'Přidat');
+
+        $form->onSuccess[] = [$this, 'commentFormSucceeded'];
+        return $form;
+    }
+
+    public function commentFormSucceeded($form, $values)
+    {
+
+        $this->database->table('knihy')->insert([
+            'nazev_knihy' => $values->nazev,
+            'autor' => $values->autor,
+            'zanr_id' => $values->zanr_id,
+        ]);
+
+        $this->flashMessage('Kniha přidána', 'success');
+        $this->redirect('this');
+    }
+
+}
